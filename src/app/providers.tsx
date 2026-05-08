bd146ec2fb798c00ce4d78528a94d3e7f42ce7e5
Gustavo Salas <lordtavo27@gmail.com>
2026-05-08 17:01:08 -0500

    feat: Implemented token logic to pay

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 60 * 8, // 8 horas
+      },
+    },
+  }));
 
   return (
     <QueryClientProvider client={queryClient}>
